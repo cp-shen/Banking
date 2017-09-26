f2557e08236ac340b25d09d9b973cb1d249d3cb1
cp-shen <cp_shen@outlook.com>
2017-09-26 15:24:08 +0800

    123

---
+++ src/banking/domain/SavingsAccount.java
@@ -0,0 +1,17 @@
+package banking.domain;
+
+import banking.domain.Account;
+
+/**
+ * @author BJTU16301130
+ * @version 201270922
+ */
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(double balance, double interest_rate){
+        super(balance);
+        interestRate = interest_rate;
+    }
+}
+
